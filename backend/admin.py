5684283dc990de0fc8a4e24d51c017519bcfc1d0
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2025-04-07 12:33:33 -0300

    Adicionando models no admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (Atividade, Clube, Equipamento, Perfil, Record)
 
 # Register your models here.
+
+admin.site.register(Atividade)
+admin.site.register(Clube)
+admin.site.register(Equipamento)
+admin.site.register(Perfil)
+admin.site.register(Record)
